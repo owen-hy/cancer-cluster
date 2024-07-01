ce1eaa10beda8a813a39b8b3d5920d762ae48f5f
owenhy <owenhy@umich.edu>
2024-07-01 16:42:23 -0400

    began data loading

---
+++ data_load.R
@@ -0,0 +1,12 @@
+# loading in datasets and necessary libraries
+# MItools is downloaded via devtools::install_github("junsoukchoi/MItools")
+library(tidyverse)
+library(MItools)
+
+lungData <- read_rds("./lungData.RDS")
+lung_cell <- lungData$lung_cells
+lung_meta <- lungData$lung_metadata
+
+ovarianData <- read_rds("./ovarianData.RDS")
+ovarian_cell <- ovarianData$ovarian_cells
+ovarian_meta <- ovarianData$ovarian_metadata
