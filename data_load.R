1118b3c262e429ea0721a87251d6b757ef325b2a
claracraiu <clara.maria.craiu@gmail.com>
2024-07-02 16:30:52 -0400

    Update data_load.R
    created filtered lung combined data set

--- data_load.R
+++ data_load.R
@@ -13,4 +13,12 @@ ovarian_cell <- ovarianData$ovarian_cells
 ovarian_meta <- ovarianData$ovarian_metadata
 ovarian_combined <- merge(ovarianData$ovarian_cells, ovarianData$ovarian_metadata)
 
+lung_filtered <- lung_combined %>% 
+  group_by(patient_id, image_id) %>% 
+  count() %>%
+  group_by(patient_id) %>% 
+  arrange(desc(n)) %>% 
+  distinct(patient_id, .keep_all = TRUE) %>% 
+  left_join(lung_combined[ , c("image_id" , "slide_id", "x", "y", "pheno", "gender", "mhcII_status", "age_at_diagnosis", "stage_numeric", "pack_years", "survival_days", "cause_of_death", "adjuvant_therapy", "time_to_recurrence_days", "recurrence_or_lung_ca_death")], by = "image_id")
+
 ## Calculating K and K cross
