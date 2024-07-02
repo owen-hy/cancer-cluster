c59f434b5621e4b2d0645976588aebe2db27f327
claracraiu <clara.maria.craiu@gmail.com>
2024-07-02 16:15:52 -0400

    test

--- data_load.R
+++ data_load.R
@@ -6,9 +6,11 @@ library(MItools)
 lungData <- read_rds("./lungData.RDS")
 lung_cell <- lungData$lung_cells
 lung_meta <- lungData$lung_metadata
+lung_combined <- merge(lungData$lung_cells, lungData$lung_metadata)
 
 ovarianData <- read_rds("./ovarianData.RDS")
 ovarian_cell <- ovarianData$ovarian_cells
 ovarian_meta <- ovarianData$ovarian_metadata
+ovarian_combined <- merge(ovarianData$ovarian_cells, ovarianData$ovarian_metadata)
 
 ## Calculating K and K cross
