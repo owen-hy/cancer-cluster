a26aff714c8a3d89ef9e66d8173d522828ad250a
ckh2226 <hungck22@wfu.edu>
2024-07-09 16:18:42 -0400

    Update clustering-eda.R

--- clustering-eda.R
+++ clustering-eda.R
@@ -42,9 +42,9 @@ pca_lung40 <- prcomp(new_lung_meta40)
 summary(pca_lung40)
 
 fviz_nbclust(
-  pca_lung40$x[,1:7],
+  pca_lung40$x[,1:5],
   FUNcluster = hcut,
-  method = c("silhouette"),
+  method = c("wss"),
   diss = NULL,
   k.max = 10,
   nboot = 100,
@@ -55,8 +55,18 @@ fviz_nbclust(
   print.summary = TRUE
 )
 
-kmeans_lung40 <- kmeans(data.frame(pca_lung40$x[, 1:7]), centers = 2)
+hier_clust <- hcut(pca_lung40$x[,1:5], k = 2)
+plot(hier_clust)
+rect.hclust(hier_clust, k = 2)
+  
+  
+
+fviz_silhouette((hier_clust))
+
+kmeans_lung40 <- kmeans(data.frame(pca_lung40$x[, 1:5]), centers = 2)
+kmeans_lung40_e <- eclust(pca_lung40$x[, 1:5], "kmeans", k = 2, nstart = 100)
 fviz_pca_ind(pca_lung40, habillage = kmeans_lung40$cluster, addEllipses = TRUE)
+fviz_silhouette(kmeans_lung40_e)
 
 lung_meta40_eda <- lung_meta40 |>
   filter(!is.na(p_CK) & !is.na(p_CD8) & !is.na(p_CD14) & !is.na(p_Other) & !is.na(p_CD19) & !is.na(p_CD4)
@@ -66,7 +76,15 @@ lung_meta40_eda <- lung_meta40 |>
          & !is.na(k_CD14_Other) & !is.na(k_CD14_CD19) & !is.na(k_CD14_CD4) & !is.na(k_Other_CD19) & 
            !is.na(k_Other_CD4) & !is.na(k_CD19_CD4))
 
-lung_meta40_eda$cluster_id <- kmeans_lung40$cluster
+lung_meta40_eda$cluster_id <- hier_clust$cluster
+lung_meta40_eda |>
+  group_by(cluster_id) |>
+  count()
+
+data <- lung_meta40_eda[, sapply(lung_meta40_eda, is.numeric)]
+cluster_meds <- data |>
+  group_by(cluster_id) |>
+  summarize(across(everything(), median, na.rm = TRUE))
 
 # Summary statistics
 ## Number of patients in each cluster
@@ -92,3 +110,81 @@ lung_meta40_eda |>
             prop_stage3 = sum(stage_numeric == 3)/n(),
             prop_stage4 = sum(stage_numeric == 4)/n()) # higher proportion of stage 2 patients in cluster 2
 
+
+# Spectral Clustering
+
+## Create Similarity matrix of Euclidean distance between points
+S <- as.matrix(dist(pca_lung40$x[,1:5]))
+
+
+## Create Degree matrix
+D <- matrix(0, nrow=nrow(pca_lung40$x[,1:5]), ncol = nrow(pca_lung40$x[,1:5])) # empty nxn matrix
+
+for (i in 1:nrow(pca_lung40$x[,1:5])) {
+  
+  # Find top 10 nearest neighbors using Euclidean distance
+  index <- order(S[i,])[2:11]
+  
+  # Assign value to neighbors
+  D[i,][index] <- 1 
+}
+
+# find mutual neighbors
+D = D + t(D) 
+D[ D == 2 ] = 1
+
+# find degrees of vertices
+degrees = colSums(D) 
+n = nrow(D)
+
+
+## Compute Laplacian matrix
+# Since k > 2 clusters (3), we normalize the Laplacian matrix:
+laplacian = ( diag(n) - diag(degrees^(-1/2)) %*% D %*% diag(degrees^(-1/2)) )
+eigen(laplacian)
+
+which.max(diff(sort(eigen(laplacian)$values, decreasing = F))) + 1
+
+
+## Compute eigenvectors
+eigenvectors = eigen(laplacian, symmetric = TRUE)
+n = nrow(laplacian)
+eigenvectors = eigenvectors$vectors[,(n - 2):(n - 1)]
+
+
+set.seed(1748)
+## Run Kmeans on eigenvectors
+sc = kmeans(eigenvectors, 4)
+
+
+## Pull clustering results
+sc_results = cbind(pca_lung40$x[,1:5], cluster = as.factor(sc$cluster))
+head(sc_results)
+
+as.data.frame(sc_results) |>
+  group_by(cluster) |>
+  count()
+
+ggplot(data = sc_results, aes(x = PC1, y = PC2)) + aes(color = factor(cluster)) + geom_point()
+
+lung_meta40_eda_spectral <- lung_meta40 |>
+  filter(!is.na(p_CK) & !is.na(p_CD8) & !is.na(p_CD14) & !is.na(p_Other) & !is.na(p_CD19) & !is.na(p_CD4)
+         & !is.na(k_CK) & !is.na(k_CD8) & !is.na(k_CD14) & !is.na(k_Other) & !is.na(k_Other) & !is.na(k_CD19)
+         & !is.na(k_CD4) & !is.na(k_CK_CD8) & !is.na(k_CK_CD14) & !is.na(k_CK_Other) & !is.na(k_CK_CD19) & 
+           !is.na(k_CK_CD4) & !is.na(k_CD8_CD14) & !is.na(k_CD8_Other) & !is.na(k_CD8_CD19) & !is.na(k_CD8_CD4)
+         & !is.na(k_CD14_Other) & !is.na(k_CD14_CD19) & !is.na(k_CD14_CD4) & !is.na(k_Other_CD19) & 
+           !is.na(k_Other_CD4) & !is.na(k_CD19_CD4))
+
+clusters <- sc_results[, 6]
+
+lung_meta40_eda_spectral$cluster_id <- clusters
+lung_meta40_eda_spectral |>
+  group_by(cluster_id) |>
+  count()
+
+data <- lung_meta40_eda_spectral[, sapply(lung_meta40_eda_spectral, is.numeric)]
+cluster_meds_spectral <- data |>
+  group_by(cluster_id) |>
+  summarize(across(everything(), median, na.rm = TRUE))
+
+fviz_silhouette(silhouette(sc$cluster, dist(eigenvectors)))
