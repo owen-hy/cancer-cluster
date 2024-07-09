abec99c106f8a74b2bb1dea4033ed0cd096b416c
claracraiu <clara.maria.craiu@gmail.com>
2024-07-09 14:32:02 -0400

    Update test_cluster_and_validation.R

--- test_cluster_and_validation.R
+++ test_cluster_and_validation.R
@@ -5,17 +5,14 @@
 
 # creating distance matrix (method = euclidean) 
 distance_mat <- dist(new_lung_meta40)
-# heirarchical clustering 
-heir_clust <- hclust(distance_mat, method = 'average')
-heir_clust2 <- hclust(distance_mat, method = 'ward.D')
-plot(heir_clust)
-plot(heir_clust2)
+# hierarchical clustering 
+hier_clust <- hclust(distance_mat, method = 'ward.D')
+plot(hier_clust)
 #splitting into clusters 
 rect.hclust(heir_clust, k = 3)
-rect.hclust(heir_clust2, k = 3)
 
 # K-means clustering
-k_means_lung <- eclust(new_lung_meta40, "kmeans", k = 3, nstart = 100)
+k_means_lung <- eclust(new_lung_meta40, "kmeans", k = 4, nstart = 100)
 # before I did it this way 
 # k_mean_vals_40 = kmeans(new_lung_meta40, centers = 3, nstart = 100)
 
@@ -24,10 +21,15 @@ sil <- silhouette(k_means_lung$cluster, dist(new_lung_meta40))
 plot(sil)
 fviz_silhouette(k_means_lung)
 
-# enhanced heirarchical clustering 
-heir_clust_lung <- eclust(new_lung_meta40, "hclust", k = 3, method = "ward.D", graph = FALSE)
+# enhanced hierarchical clustering 
+hier_clust_lung <- eclust(lung_transform3, FUNcluster = "hclust", k = 2, hc_method = "ward.D", graph = FALSE)
 
-sil2 <- silhouette(heir_clust_lung$cluster, dist(new_lung_meta40))
+sil2 <- silhouette(hier_clust_lung$cluster, dist(new_lung_meta40))
 sil2
 plot(sil2)
-fviz_silhouette(heir_clust_lung)
+fviz_silhouette(hier_clust_lung)
+
+
+View(lung_transform)
+View(new_lung_meta40)
+??eclust
