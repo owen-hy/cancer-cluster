cb623c8f8c32248a170c28fc06bde3a891e89d3e
claracraiu <clara.maria.craiu@gmail.com>
2024-07-11 15:18:02 -0400

    Update heatmap.R

--- heatmap.R
+++ heatmap.R
@@ -6,8 +6,10 @@ lung_meta40_eda$cluster_id <- k_means_lung$cluster
 file_plot <- lung_meta40_eda |>
   arrange(cluster_id)
 
-clust <- file_plot |> select(cluster_id) |>
+clust <- file_plot |> 
+  select(cluster_id) |>
   mutate(cluster_id = factor(cluster_id))
+
 rownames(clust) = rownames(file_plot)
 
 heat_df <- file_plot[, sapply(file_plot, is.numeric)] |>
@@ -22,8 +24,7 @@ rownames(clust)
 pheatmap(lung_meta40_eda, annotation_row = clust, cluster_cols = FALSE, cluster_rows = FALSE)
 
 ??pheatmap
-pheatmap(, annotation_row = clust, cluster_cols = FALSE, cluster_rows = FALSE)
-hmapSpectra(heat_df)
+
 View(file_plot)
 View(clust)
 View(lung_meta40_eda)
