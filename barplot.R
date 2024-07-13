2497d27b425dfa098514c759911ad337eba19521
owenhy <owenhy@umich.edu>
2024-07-13 13:29:26 -0400

    finished barplots

---
+++ barplot.R
@@ -0,0 +1,34 @@
+## Barplots 
+
+current_data <- lung_meta40_hier
+
+current_data <- current_data |> select_if(~!is.numeric(.)) |>
+  mutate(stage_numeric = lung_meta40_hier$stage_numeric,
+         #Change based on which cluster
+         cluster_id = lung_meta40_hier$cluster_id_2) |>
+  select(-image_id, -patient_id, -cause_of_death, -stage_at_diagnosis) 
+
+current_data |>
+  select(cluster_id, gender) |>
+  ggplot(aes(x = factor(cluster_id), fill = gender)) +
+  geom_bar(position = 'dodge')
+
+current_data |>
+  select(cluster_id, mhcII_status) |>
+  ggplot(aes(x = factor(cluster_id), fill = mhcII_status)) +
+  geom_bar(position = 'dodge')
+
+current_data |>
+  select(cluster_id, adjuvant_therapy) |>
+  ggplot(aes(x = factor(cluster_id), fill = adjuvant_therapy)) +
+  geom_bar(position = 'dodge')
+
+current_data |>
+  select(cluster_id, stage_numeric) |>
+  ggplot(aes(x = factor(cluster_id), fill = factor(stage_numeric))) +
+  geom_bar(position = 'dodge')
+
+
+
+
+  
