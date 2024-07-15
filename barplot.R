435500b0144a91d41e1f95d42d4779f528fe8499
owenhy <owenhy@umich.edu>
2024-07-15 15:42:19 -0400

    changes to barplot

--- barplot.R
+++ barplot.R
@@ -1,5 +1,7 @@
 ## Barplots 
 
+### If you want side by side, replace position = 'fill' with position = 'dodge
+
 current_data <- lung_meta40_hier
 
 current_data <- current_data |> select_if(~!is.numeric(.)) |>
@@ -11,22 +13,22 @@ current_data <- current_data |> select_if(~!is.numeric(.)) |>
 current_data |>
   select(cluster_id, gender) |>
   ggplot(aes(x = factor(cluster_id), fill = gender)) +
-  geom_bar()
+  geom_bar(position = 'fill')
 
 current_data |>
   select(cluster_id, mhcII_status) |>
   ggplot(aes(x = factor(cluster_id), fill = mhcII_status)) +
-  geom_bar(position = 'dodge')
+  geom_bar(position = 'fill')
 
 current_data |>
   select(cluster_id, adjuvant_therapy) |>
   ggplot(aes(x = factor(cluster_id), fill = adjuvant_therapy)) +
-  geom_bar(position = 'dodge')
+  geom_bar(position = 'fill')
 
 current_data |>
   select(cluster_id, stage_numeric) |>
   ggplot(aes(x = factor(cluster_id), fill = factor(stage_numeric))) +
-  geom_bar(position = 'dodge')
+  geom_bar(position = 'fill')
 
 
 
